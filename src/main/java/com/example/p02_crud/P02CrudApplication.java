3823472edf0d97b8dfe5979068e265b42a7d6c6c
Administrateur <wcaillere.pro@gmail.com>
2023-05-30 13:48:15 +0200

    Ajout connexion hibernate

---
+++ src/main/java/com/example/p02_crud/P02CrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.p02_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P02CrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P02CrudApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/p02_crud/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.p02_crud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String Home() {
+        return "home";
+    }
+}
