5b002af3b4054cc6c00b9c71416f1e5b21ca3a37
Administrateur <wcaillere.pro@gmail.com>
2023-06-01 13:42:30 +0200

    Envoie informations formulaire au controller de sauvegarde

--- src/main/java/com/example/p02_crud/controller/HomeController.java
+++ src/main/java/com/example/p02_crud/controller/HomeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -26,16 +28,24 @@ public class HomeController {
     }
 
     @GetMapping("/addTeacher")
-    public String AddTeacher() {
+    public String AddTeacher(Model model) {
+        model.addAttribute("teacher", new DTOTeacher());
+
         return "fragments/addTeacher";
     }
 
     @GetMapping("/listTeacher")
-    public String listTeacher(Model model) {
+    public String ListTeacher(Model model) {
         // Récupérer les données à afficher
         List<DTOTeacher> teachers = st.TeachersList();
         model.addAttribute("teachersList", teachers);
 
         return "fragments/listTeacher";
+    }
+
+    @PostMapping("/saveTeacher")
+    public String SaveTeacher(@RequestParam Object test) {
+        System.out.println(test);
+        return "fragments/saveConfirm";
     }
 }
